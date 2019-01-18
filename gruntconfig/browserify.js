5989c73180dc824fc9849174f75fd66f5b24f597
Brandon Clayton <bclayton@usgs.gov>
2019-01-18 15:59:32 -0700

    Update grunt config

--- gruntconfig/browserify.js
+++ gruntconfig/browserify.js
@@ -2,19 +2,34 @@
 
 const config = require('./config');
 
+const EXPORTS = [
+  'lib/Devices',
+  'lib/Footer',
+  'lib/Header',
+  'lib/Tools',
+  'lib/Spinner'
+].map((path) => `./${config.htdocs}/js/${path}:${path}`).concat([
+  'd3',
+  'bootstrap',
+  'jquery',
+  'axios'
+]);
+
+
 const browserify = {
   options: {
     browserifyOptions: {
       paths: [
-        config.htdocs, 
+        `./${config.htdocs}`, 
       ]
     }
   },
   source: {
-    src: [
-      `${config.src}/**/*.js`,
-    ],
+    src: [  ],
     dest: `${config.build}/${config.src}/${config.mainJS}`,
+    options: {
+      alias: EXPORTS
+    }
   }
 };
 
--- gruntconfig/index.js
+++ gruntconfig/index.js
@@ -1,4 +1,5 @@
 'use strict';
+
 const grunt = require('grunt');
 
 const config = {
